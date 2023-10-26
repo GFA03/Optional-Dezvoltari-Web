f3a0d36c034f4d8049f9a35013a6c64765b19289
Gavrila Alexandru <florinalexandru302003@gmail.com>
2023-10-26 19:39:24 +0300

    tested sql server database

--- Lab2_24/Lab2_24/Controllers/StudentsController.cs
+++ Lab2_24/Lab2_24/Controllers/StudentsController.cs
@@ -1,5 +1,6 @@
 ﻿using Lab2_24.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Lab2_24.Controllers
@@ -12,7 +13,7 @@ namespace Lab2_24.Controllers
         {
             new Student { Id = 1, Name = "Ana", Age = 21},
             new Student { Id = 2, Name = "Maria", Age = 19},
-            new Student { Id = 1, Name = "Vlad", Age = 22},
+            new Student { Id = 3, Name = "Vlad", Age = 22},
             new Student { Id = 4, Name = "Florin", Age = 25},
             new Student { Id = 5, Name = "Marian", Age = 20},
         };
@@ -31,6 +32,48 @@ namespace Lab2_24.Controllers
             return students.OrderBy(o => o.Name).ToList();
         }
 
+        [HttpGet("byId")]
+        public Student Get(int id)
+        {
+            return students.FirstOrDefault(s => s.Id.Equals(id));
+        }
+
+        [HttpGet("byId/{id}")]
+        public Student GetByIdEndpoint(int id)
+        {
+            return students.FirstOrDefault(s => s.Id.Equals(id));
+        }
+
+        [HttpGet("filter/{name}/{age}")]
+        public Student GetWithFilters(string name, int age)
+        {
+            return students.FirstOrDefault(s => s.Name.Equals(name) && s.Age.Equals(age));
+        }
+
+        [HttpGet("fromRouteWithId/{id}")]
+        public Student GetByIdWithFromRoute([FromRoute] int id)
+        {
+            return students.FirstOrDefault(s => s.Id.Equals(id));
+        }
+
+        [HttpGet("fromHeader")]
+        public Student GetByIdWithFromHeader([FromHeader] int id)
+        {
+            return students.FirstOrDefault(s => s.Id.Equals(id));
+        }
+
+        [HttpGet("fromQuery")]
+        public Student GetByIdWithFromQuery([FromQuery] int id)
+        {
+            return students.FirstOrDefault(s => s.Id.Equals(id));
+        }
+
+        [HttpGet("fromQueryAsync")]
+        public async Task<Student> GetByIdWithFromQueryAsync([FromQuery] int id)
+        {
+            return students.FirstOrDefault(s => s.Id.Equals(id));
+        }
+
         // Create
 
         [HttpPost]
@@ -40,6 +83,21 @@ namespace Lab2_24.Controllers
             return students;
         }
 
+        [HttpPost("fromBody")]
+        public IActionResult AddWithFromBody([FromBody] Student student)
+        {
+            students.Add(student);
+            return Ok(students);
+        }
+
+        [HttpPost("fromForm")]
+        public IActionResult AddWithFromForm([FromForm] Student student)
+        {
+            students.Add(student);
+            return Ok(students);
+        }
+
+        // Delete
 
         [HttpDelete]
         public List<Student> Delete(Student student)
@@ -63,5 +121,32 @@ namespace Lab2_24.Controllers
             }
             return students;
         }
+
+        // Update
+
+        //Partial Update
+        [HttpPatch]
+        public IActionResult Patch([FromRoute] int id, [FromBody] JsonPatchDocument<Student> student) 
+        { 
+            if(student != null)
+            {
+                var studentToUpdate = students.FirstOrDefault(s => s.Id.Equals(id));
+                student.ApplyTo(studentToUpdate,(Microsoft.AspNetCore.JsonPatch.Adapters.IObjectAdapter) ModelState);
+
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                else return Ok(students);
+
+            }
+            return BadRequest("Object is null");
+
+/*            return NotFound();
+            return NoContent();
+            return Forbid();*/
+        }
+
+
     }
 }
